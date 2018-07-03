ad22bb24d012f7cb6ddf2de995f22956cc2ecf46
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-07-03 13:30:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# get-latest-version
+
+Find the latest version of an npm module matching a given semver range
+
+[![npm version](https://img.shields.io/npm/v/get-latest-version.svg?style=flat-square)](http://browsenpm.org/package/get-latest-version)[![Build Status](https://img.shields.io/travis/rexxars/get-latest-version/master.svg?style=flat-square)](https://travis-ci.org/rexxars/get-latest-version)
+
+## Installing
+
+```
+npm install --save get-latest-version
+```
+
+## Basic usage
+
+```js
+const getLatestVersion = require('get-latest-version')
+
+getLatestVersion('some-module')
+  .then(version => console.log(version)) // 1.0.0, or whichever is 'latest'
+  .catch(err => console.error(err))
+
+getLatestVersion('some-other-module', {range: '^1.0.0'})
+  .then(version => console.log(version)) // highest version matching ^1.0.0 range
+  .catch(err => console.error(err))
+```
+
+## Disabling authenticated requests
+
+By default, this module will read the authorization token for whichever registry the module belongs to, based on local npm configuration. To disable this and always send unauthenticated requests to the registry, provide `auth: false` to the options:
+
+```js
+getLatestVersion('some-module', {auth: false})
+  .then(version => console.log(version))
+  .catch(err => console.error(err))
+```
+
+## Developing
+
+```bash
+git clone git@github.com:rexxars/get-latest-version.git
+cd get-latest-version
+npm install
+npm test
+```
+
+## License
+
+MIT © [Espen Hovlandsdal](https://espen.codes/)
---
+++ src/index.js
@@ -0,0 +1,84 @@
+const url = require('url')
+const getIt = require('get-it')
+const {debug, retry, promise, httpErrors, jsonResponse} = require('get-it/middleware')
+const registryUrl = require('registry-url')
+const registryAuthToken = require('registry-auth-token')
+const semver = require('semver')
+
+const isJson = contentType => /(application\/json|\+json)/.test(contentType || '')
+
+const shouldRetry = (err, num, options) => {
+  const response = err.response || {statusCode: 500, headers: {}}
+
+  // allow retries on low-level errors (socket errors et al)
+  return (
+    retry.shouldRetry(err, num, options) ||
+    // npm registry routinely fails, giving 503 and similar
+    (response && response.statusCode >= 500) ||
+    // npm registry sometimes returns 2xx with HTML content
+    (response.statusCode < 300 && !isJson(response.headers['content-type']))
+  )
+}
+
+const httpRequest = getIt([
+  jsonResponse({force: true}),
+  httpErrors(),
+  debug({namespace: 'get-latest-version'}),
+  promise(),
+  retry({shouldRetry})
+])
+
+const getLatestVersion = (pkgName, opts) => {
+  const scope = pkgName.split('/')[0]
+  const regUrl = registryUrl(scope)
+  const pkgUrl = url.resolve(regUrl, encodeURIComponent(pkgName).replace(/^%40/, '@'))
+  const options =
+    typeof opts === 'string'
+      ? {range: opts, auth: true}
+      : Object.assign({range: 'latest', auth: true}, opts)
+
+  const authInfo = options.auth && registryAuthToken(regUrl, {recursive: true})
+  const request = options.request || httpRequest
+
+  const headers = {
+    accept: 'application/vnd.npm.install-v1+json; q=1.0, application/json; q=0.8, */*'
+  }
+
+  if (authInfo) {
+    headers.authorization = `${authInfo.type} ${authInfo.token}`
+  }
+
+  return request({url: pkgUrl, headers})
+    .then(res => {
+      const data = res.body
+      const range = options.range
+
+      if (data['dist-tags'][range]) {
+        return data['dist-tags'][range]
+      }
+
+      if (data.versions[range]) {
+        return range
+      }
+
+      const versions = Object.keys(data.versions)
+      const version = semver.maxSatisfying(versions, range)
+
+      if (!version) {
+        throw new Error(`No version exists that satisfies "${range}"`)
+      }
+
+      return version
+    })
+    .catch(err => {
+      if (err.response && err.response.statusCode === 404) {
+        throw new Error(`Package \`${pkgName}\` doesn't exist`)
+      }
+
+      throw err
+    })
+}
+
+getLatestVersion.request = httpRequest
+
+module.exports = getLatestVersion
